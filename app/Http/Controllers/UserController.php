fe6907f5145ddc044c24a247130f4029a72dd6f9
Paul Stan <Paul Stan>
2023-05-19 10:46:59 +0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,7 +27,6 @@ class UserController extends Controller
         return response()->json([
             'Status' => 'Ok',
             'NrUsers' => $users,
-            'NewProp' => true,
         ], 200);
     }
 
