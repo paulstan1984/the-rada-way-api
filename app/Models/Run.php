4fa025516ffa1c84f9c0c436b758ff853e18a487
Paul Stan <Paul Stan>
2023-06-09 20:33:07 +0300

    Update Run.php

--- app/Models/Run.php
+++ app/Models/Run.php
@@ -13,13 +13,4 @@ class Run extends Model
     var $fillable = ['user_id', 'startTime', 'endTime', 'distance', 'avgSpeed', 'locations'];
     var $hidden = ['created_at', 'updated_at'];
 
-    #region accesors
-    protected function locations(): Attribute
-    {
-        return Attribute::make(
-            get: fn ($value) => json_decode($value),
-            set: fn ($value) => json_encode($value)
-        );
-    }
-    #endregion
 }
