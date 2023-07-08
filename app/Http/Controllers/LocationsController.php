7eda01979b4814295435ec1ea23b627f9738b32b
Paul Stan <Paul Stan>
2023-07-08 19:27:45 +0300

    Update LocationsController.php

--- app/Http/Controllers/LocationsController.php
+++ app/Http/Controllers/LocationsController.php
@@ -19,7 +19,7 @@ class LocationsController extends Controller
     {
         $this->repository = $repository;
         $this->paginationService = $paginationService;
-        $this->paginationService->PageSize = 200;
+        $this->paginationService->PageSize = 400;
     }
 
     public function search(Request $request, $page, $run_id): JsonResponse
