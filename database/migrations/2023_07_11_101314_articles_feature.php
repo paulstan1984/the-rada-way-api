f4600f1be1194637dcfbc749814b9518394c86a6
Paul Stan <Paul Stan>
2023-07-11 10:28:39 +0300

    Update 2023_07_11_101314_articles_feature.php

---
+++ database/migrations/2023_07_11_101314_articles_feature.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 50);
+            $table->string('imagelink', 100);
+        });
+
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 100);
+            $table->string('description', 1000);
+            $table->string('imagelink', 100);
+            $table->string('link', 100);
+        });
+
+        Schema::table('articles', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id');
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+        Schema::dropIfExists('categories');
+    }
+};
