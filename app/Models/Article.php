adb95b5638586154f07574856e80ca6c15c8be4d
Paul Stan <Paul Stan>
2023-07-12 10:38:47 +0300

    Update Article.php

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -11,7 +11,7 @@ class Article extends Model
     use HasFactory;
 
     var $fillable = ['title', 'description', 'link', 'imagelink', 'category_id', 'position'];
-    var $hidden = ['created_at', 'updated_at'];
+    var $hidden = ['updated_at'];
 
     protected $casts = [
         'created_at' => MyDateTime::class,
